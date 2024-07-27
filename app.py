42c1ba41b44931a392866e93a176fba52baadf44
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2024-07-26 20:53:52 -0400

    defines and loads API key from .env, defines /convert route, root route loads index.html

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 import requests
-from flask import Flask
+from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
 
 app = Flask(__name__)
 
+URL = 'http://data.fixer.io/api/'
+API_KEY = os.getenv('access_key')
+
 @app.route('/')
 def home():
-    return "Hello, World!"
+    render_template('index.html')
+
+@app.route('/convert', methods=['POST'])
+def conversion():
+    pass
 
 if __name__ == '__main__':
     app.run()
